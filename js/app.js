eeae65503d7907bf7ef54e78e8c40e4146df1836
bharadwaj.turlapati <bharadwaj.turlapati@kony.com>
2016-02-17 21:30:22 +0530

    commiting new set of code. 1. Including the polymer beautification

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/*c3.generate({
+	bindto: '#chart_other',
+    data: {
+        columns: [
+            ['data',50]
+        ],
+        type: 'gauge',
+    },
+    gauge: {
+
+    },
+    color: {
+        pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'],
+        values: [30, 60, 90, 100]
+    },
+    size: {
+        height: 180
+    }
+});*/
